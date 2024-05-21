481636808b9c9011ad95e4a85302833be2082dee
Gautam0610 <167000391+Gautam0610@users.noreply.github.com>
2024-05-21 16:21:44 +0530

    Add files via upload
    A weather forecasting app built with advanced Java leverages the language's robust features to deliver accurate and intuitive weather predictions. It employs sophisticated algorithms to process meteorological data, providing users with real-time updates, detailed forecasts, and customizable notifications. The app's user interface is designed for seamless navigation, offering interactive maps, charts, and graphs to visualize weather patterns.

---
+++ WeatherForcastingApp/src/main/java/MyPackage/HttpServletResponse.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class HttpServletResponse {
+
+	public HttpServletResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
